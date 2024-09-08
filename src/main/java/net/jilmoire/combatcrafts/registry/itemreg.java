89a9e91ad73b71fb6da110ed1b88f50d08d001a5
gmlopez <guilmarlopez2007@gmail.com>
2024-09-09 07:37:33 +0800

    Removal of lang and item model dir in preparation for Datagen

--- src/main/java/net/jilmoire/combatcrafts/registry/itemreg.java
+++ src/main/java/net/jilmoire/combatcrafts/registry/itemreg.java
@@ -6,15 +6,17 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public class itemreg {
+    ///Logger for item registry.
     public static void itemlogger() {
         CombatCrafts.LOGGER.info("Registering Items for " + CombatCrafts.MOD_ID);
     }
-
+    ///Method for registering items.
     public static <I extends Item> I registerItem(String name, I item) {
         return Registry.register(Registries.ITEM, CombatCrafts.id(name), item);
     }
-
+    ///Items will call the method and be added in the game.
     public static final Item EXAMPLE_ITEM = (registerItem("example_item", new Item(new Item.Settings())));
-    /// Watch datagen tutorial before adding other shits and giggles
+
+    ///Loads the entire script.
     public static void itemload(){}
 }
