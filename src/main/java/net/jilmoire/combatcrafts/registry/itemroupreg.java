185a64544c6a31c54ae166f9b9cf516d9b7141dd
gmlopez <guilmarlopez2007@gmail.com>
2024-09-09 08:10:31 +0800

    Creation of Creative Menu for Mod Items

---
+++ src/main/java/net/jilmoire/combatcrafts/registry/itemroupreg.java
@@ -0,0 +1,8 @@
+package net.jilmoire.combatcrafts.registry;
+
+import net.jilmoire.combatcrafts.CombatCrafts;
+import net.minecraft.text.Text;
+
+public class itemroupreg {
+    private static final Text EXAMPLE_TITLE = Text.translatable("itemGroup." + CombatCrafts.MOD_ID + ".examplegroup");
+}
